c3b70e67211d830101205e9821d09c880bcb0699
Samuel Abu <abu.sammie@gmail.com>
2023-07-26 19:32:27 +0200

    Home UI progress

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, SafeAreaView, ScrollView, Dimensions } from 'react-native'
+//import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import HeaderTab from '../components/HeaderTab'
+import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
+import RestaurantItem from '../components/RestaurantItem'
+import Constants from 'expo-constants'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const Home = () => {
     return (
-        <SafeAreaView>
-            <HeaderTab />
+        <SafeAreaView style={{ backgroundColor: '#eee', height: Dimensions.get("screen").height}}>
+            <View style={{ marginTop: Constants.statusBarHeight }}>
+                <View style={{ backgroundColor: "white", padding: 15 }}>
+                    <HeaderTab />
+                    <SearchBar />
+                </View> 
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Categories />
+                <RestaurantItem />
+                <RestaurantItem />
+                <RestaurantItem />
+                <RestaurantItem />
+            </ScrollView>
         </SafeAreaView>
     )
 }
